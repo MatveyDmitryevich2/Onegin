1a2a35fe1b92bb40ac2d04d7c3a99af0fbd5b5a5
MatveyDmitryevich2 <kazancevmd0603@gmail.com>
2024-09-10 00:39:43 +0500

    Add files via upload

--- qwerty.cpp
+++ qwerty.cpp
@@ -1,67 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <assert.h>
 
 void Sortirovka (char** Massiv_ukazatelelei_na_stroki, size_t razmer);
-void Vivod_konca (char** Massiv_ukazatelelei_na_stroki, size_t razmer);
+void Vivod_konca (char** Massiv_ukazatelelei_na_stroki, size_t razmer, FILE *szhatiy);
 void Cvap (char** chto_menat, char** na_chto_menat);
+void Delaet_bykvi_malenkimi(char** Massiv_ukazatelelei_na_stroki, size_t razmer);
 
 int main(void)
 {
-    // FILE *nachalniy = fopen("Evgeniy.txt", "r");
-    // FILE *szhatiy = fopen("EvgeniyPEREMENA.txt", "w");
-    // char dlina_peremeni[9] = {0};
+    FILE *nachalniy = fopen("Evgeniy.txt", "r");
+    FILE *szhatiy   = fopen("EvgeniyPEREMENA.txt", "w");
+    const size_t razmer = 2000;
+    int zapolneniy_razmer = 0;
 
-    // int** Massiv_ukazatelei = (int **)calloc(7, sizeof(char *));
-    // char* bufer = 0;
+    if (nachalniy == nullptr || szhatiy == nullptr)
+    {
+        fprintf(stderr, "Файл не открылся, иди читай книгу");
+    }
 
-    //     for (int i = 0; i < 7; i++)
-    //     {
-    //         fgets(Massiv_ukazatelei[i], 6, nachalniy);
-    //     }
-    // //семь на восемь восемь насемь мы фопфов отпидарасим
-    // for (int i = 0; i < 7; i++)
-    // {
-    //     for (int u = 0; u < 6 - 1; u++)
-    //     {
-    //         if(strcmp(stroka[u], stroka[u + 1]) < 0)
-    //         {
-    //             bufer = &stroka[u];
-    //             stroka[u] = stroka[u + 1];
-    //             stroka[u + 1] = bufer;
-    //         }
-    //     }
-    // }
-
-    const size_t razmer = 7;
     char** Massiv_ukazatelelei_na_stroki = (char **)calloc(razmer, sizeof(char *));
+    size_t musor = 0;
 
-    Massiv_ukazatelelei_na_stroki[0] = (char*)calloc(4, sizeof(char));
-    Massiv_ukazatelelei_na_stroki[1] = (char*)calloc(6, sizeof(char));
-    Massiv_ukazatelelei_na_stroki[2] = (char*)calloc(9, sizeof(char));
-    Massiv_ukazatelelei_na_stroki[3] = (char*)calloc(6, sizeof(char));
-    Massiv_ukazatelelei_na_stroki[4] = (char*)calloc(7, sizeof(char));
-    Massiv_ukazatelelei_na_stroki[5] = (char*)calloc(5, sizeof(char));
-    Massiv_ukazatelelei_na_stroki[6] = (char*)calloc(6, sizeof(char));
+    for (int i = 0; getline(Massiv_ukazatelelei_na_stroki + i, &musor, nachalniy) != EOF; i++)
+    {
+        zapolneniy_razmer++;
+    }
+
+    Delaet_bykvi_malenkimi (Massiv_ukazatelelei_na_stroki, zapolneniy_razmer);
+    Sortirovka (Massiv_ukazatelelei_na_stroki, zapolneniy_razmer);
+    Vivod_konca (Massiv_ukazatelelei_na_stroki, zapolneniy_razmer, szhatiy);
     
-    const char* stroki[razmer] = {"хуй", "пизда", "алкоголь", "дилдо", "залупа", "зима", "пирог"};
+    for (int i = 0; i <= zapolneniy_razmer; i++)
+    {
+        free(Massiv_ukazatelelei_na_stroki[i]);
+        Massiv_ukazatelelei_na_stroki[i] = NULL;
+    }
 
-    strcpy(Massiv_ukazatelelei_na_stroki[0], stroki[0]);
-    strcpy(Massiv_ukazatelelei_na_stroki[1], stroki[1]);
-    strcpy(Massiv_ukazatelelei_na_stroki[2], stroki[2]);
-    strcpy(Massiv_ukazatelelei_na_stroki[3], stroki[3]);
-    strcpy(Massiv_ukazatelelei_na_stroki[4], stroki[4]);
-    strcpy(Massiv_ukazatelelei_na_stroki[5], stroki[5]);
-    strcpy(Massiv_ukazatelelei_na_stroki[6], stroki[6]);
+    free(Massiv_ukazatelelei_na_stroki);
+    Massiv_ukazatelelei_na_stroki = NULL;
 
-    Sortirovka (Massiv_ukazatelelei_na_stroki, razmer);
-    Vivod_konca (Massiv_ukazatelelei_na_stroki, razmer);
-    
+    fclose(nachalniy);
+    fclose(szhatiy);
+
     return 0;
 }
 
-void Sortirovka (char** Massiv_ukazatelelei_na_stroki, size_t razmer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+void Sortirovka (const char** Massiv_ukazatelelei_na_stroki, size_t razmer)
 {
+    assert(Massiv_ukazatelelei_na_stroki != NULL);
+
     for (size_t u = 0; u < razmer - 1; u++)
     {
         for (size_t i = 0; i < razmer - u - 1; i++)
@@ -74,17 +77,35 @@ void Sortirovka (char** Massiv_ukazatelelei_na_stroki, size_t razmer)
     }
 }
 
-void Vivod_konca (char** Massiv_ukazatelelei_na_stroki, size_t razmer)
+void Vivod_konca (char** Massiv_ukazatelelei_na_stroki, size_t razmer, FILE *szhatiy)
 {
+    assert(Massiv_ukazatelelei_na_stroki != NULL);
+
     for (size_t i = 0; i < razmer; i++)
     {
-        puts(Massiv_ukazatelelei_na_stroki[i]);
+        fputs(Massiv_ukazatelelei_na_stroki[i], szhatiy);
     }
 }
 
-void Cvap (char** chto_menat, char** na_chto_menat)
+void Cvap (const char** chto_menat, const char** na_chto_menat)
 {
-    char* bufer = *chto_menat;
+    assert(chto_menat != NULL);
+    assert(na_chto_menat != NULL);
+
+    const char* bufer = *chto_menat;
                   *chto_menat = *na_chto_menat;
-                               *na_chto_menat = bufer;
+                                *na_chto_menat = bufer;
+}
+
+void Delaet_bykvi_malenkimi(char** Massiv_ukazatelelei_na_stroki, size_t razmer)
+{
+    assert(Massiv_ukazatelelei_na_stroki != NULL);
+
+    for (int i = 0; i < razmer; i++)
+    {
+        for (int u = 0; u < strlen(Massiv_ukazatelelei_na_stroki[i]); u++)
+        {
+            Massiv_ukazatelelei_na_stroki[i][u] = tolower(Massiv_ukazatelelei_na_stroki[i][u]);
+        }
+    }
 }
